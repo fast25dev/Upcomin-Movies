57e96c61a4bc0eec37715ef98222c4bbf1f8a018
bogibekdev <bogibekdev@gmail.com>
2023-09-19 08:28:46 +0500

    finish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "dev.fast25.upcomingmovie"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "dev.fast25.upcomingmovie"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -40,7 +40,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
